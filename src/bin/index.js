3ce8c0fdd6efa75a20777626d344c1f3f4ddba56
Jason <jasongitmail@gmail.com>
2021-07-07 06:05:50 +0000

    feat: add terminal colors where yargs allows

--- src/bin/index.js
+++ src/bin/index.js
@@ -5,17 +5,35 @@ const { hideBin } = require('yargs/helpers');
 const file = require('../lib/file');
 const project = require('../lib/project');
 const system = require('../lib/system');
+const chalk = require('chalk');
+
+const _g = chalk.green;
+const _r = chalk.reset;
+const _red = chalk.red;
 
 yargs(hideBin(process.argv))
-  .scriptName('snap')
+  .scriptName(_g('snap'))
   .usage('Usage: $0 <command> [options]')
+  .strict() // disallow superfluous args
+
+  // https://github.com/yargs/yargs/issues/199
+  // https://github.com/yargs/yargs/blob/master/locales/en.json
+  .updateStrings({
+    'Missing required argument: %s': {
+      one: _red('Missing required argument: %s'),
+    },
+    'Unknown argument: %s': {
+      one: _red('Unknown argument: %s'),
+    },
+  })
+  .demandCommand(1, _red('Please provide a command.'))
+
   .command(
     ['project [name]', 'proj [name]', 'p [name]'],
     'Create a new project',
     { name: { demand: true, string: true, hidden: true } },
     (argv) => project(argv.name)
   )
-
   .command(
     ['file [name]', 'f [name]'],
     'Create a new file & test',
@@ -25,10 +43,11 @@ yargs(hideBin(process.argv))
   .command(['system', 'sys', 's'], 'Show system info', {}, () => system())
   .alias('h', 'help')
   .alias('v', 'version')
-  .demandCommand(1, 'Please provide a command.')
-  .strict() // disallow superfluous args
+
   .epilog(
-    `Note:
+    // _r is a hack to force the terminal to retain a line break below
+    _r(
+      `Note:
   You can use "snapp" or "snap". Both CLI aliases are provided.
 
 
@@ -36,6 +55,6 @@ yargs(hideBin(process.argv))
     █ ▀ █ █ █ ▀█ █▀█
 
      MINA Protocol
-
-  `
+      `
+    )
   ).argv;
