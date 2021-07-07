be2d2224f371e13ca29a5ee1e0e1b44a7f37953e
Jason <jasongitmail@gmail.com>
2021-07-07 00:09:12 +0000

    feat: add placeholder project and file commands

--- src/bin/index.js
+++ src/bin/index.js
@@ -2,17 +2,26 @@
 
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
-const system = require('./lib/system');
+const system = require('../lib/system');
+const project = require('../lib/project');
+const file = require('../lib/file');
 
 yargs(hideBin(process.argv))
-  .scriptName('snapp')
-  .usage('Usage: $0 <cmd> [options]')
+  .scriptName('snap')
+  .usage('Usage: $0 <command> [options]')
   .command(
-    ['system', 'sys'],
-    'Show system info to provide with issue reports.',
+    ['project <name>', 'proj <name>', 'p <name>'],
+    'Create a new project.',
     {},
-    () => system()
+    (argv) => project(argv.name)
   )
+  .command(
+    ['file <name>', 'f <name>'],
+    'Create a new file & test.',
+    {},
+    (argv) => file(argv.name)
+  )
+  .command(['system', 'sys', 's'], 'Show system info.', {}, () => system())
   .alias('h', 'help')
   .alias('v', 'version')
   .demandCommand(1, 'Please provide a command.').argv;
---
+++ src/lib/project.js
@@ -0,0 +1,3 @@
+module.exports = function (name) {
+  console.log('Creating new project:', name);
+};
