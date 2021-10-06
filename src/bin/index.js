398033a9d3e61b7c7413717abd49732e57962132
Martin Minkov <minkovlmartin@gmail.com>
2021-10-06 16:19:37 -0700

    fix: Add try catch to yargs

--- src/bin/index.js
+++ src/bin/index.js
@@ -12,54 +12,59 @@ const _g = chalk.green;
 const _r = chalk.reset;
 const _red = chalk.red;
 
-yargs(hideBin(process.argv))
-  .scriptName(_g('snapp'))
-  .usage('Usage: $0 <command> [options]')
-  .strict() // disallow superfluous args
+// Try to protect against commands like `snapp <invalid-cmd> foo` which throws an error in yargs lib
+try {
+  yargs(hideBin(process.argv))
+    .scriptName(_g('snapp'))
+    .usage('Usage: $0 <command> [options]')
+    .strict() // disallow superfluous args
 
-  // https://github.com/yargs/yargs/issues/199
-  // https://github.com/yargs/yargs/blob/master/locales/en.json
-  .updateStrings({
-    'Missing required argument: %s': {
-      one: _red('Missing required argument: %s'),
-    },
-    'Unknown argument: %s': {
-      one: _red('Unknown argument: %s'),
-    },
-  })
-  .demandCommand(1, _red('Please provide a command.'))
+    // https://github.com/yargs/yargs/issues/199
+    // https://github.com/yargs/yargs/blob/master/locales/en.json
+    .updateStrings({
+      'Missing required argument: %s': {
+        one: _red('Missing required argument: %s'),
+      },
+      'Unknown argument: %s': {
+        one: _red('Unknown argument: %s'),
+      },
+    })
+    .demandCommand(1, _red('Please provide a command.'))
 
-  .command(
-    ['project [name]', 'proj [name]', 'p [name]'],
-    'Create a new project',
-    { name: { demand: true, string: true, hidden: true } },
-    async (argv) => await project(argv.name)
-  )
-  .command(
-    ['file [name]', 'f [name]'],
-    'Create a new file & test',
-    { name: { demand: true, string: true, hidden: true } },
-    (argv) => file(argv.name)
-  )
-  .command(
-    ['example [name]', 'e [name]'],
-    'Create an example project',
-    { name: { demand: true, string: true, hidden: true } },
-    async (argv) => await example(argv.name)
-  )
-  .command(['system', 'sys', 's'], 'Show system info', {}, () => system())
-  .alias('h', 'help')
-  .alias('v', 'version')
+    .command(
+      ['project [name]', 'proj [name]', 'p [name]'],
+      'Create a new project',
+      { name: { demand: true, string: true, hidden: true } },
+      async (argv) => await project(argv.name)
+    )
+    .command(
+      ['file [name]', 'f [name]'],
+      'Create a new file & test',
+      { name: { demand: true, string: true, hidden: true } },
+      (argv) => file(argv.name)
+    )
+    .command(
+      ['example [name]', 'e [name]'],
+      'Create an example project',
+      { name: { demand: true, string: true, hidden: true } },
+      async (argv) => await example(argv.name)
+    )
+    .command(['system', 'sys', 's'], 'Show system info', {}, () => system())
+    .alias('h', 'help')
+    .alias('v', 'version')
 
-  .epilog(
-    // _r is a hack to force the terminal to retain a line break below
-    _r(
-      `
+    .epilog(
+      // _r is a hack to force the terminal to retain a line break below
+      _r(
+        `
 
     █▄ ▄█ █ █▄ █ ▄▀▄
     █ ▀ █ █ █ ▀█ █▀█
 
      MINA Protocol
       `
-    )
-  ).argv;
+      )
+    ).argv;
+} catch (_err) {
+  console.log('Usage: $0 <command> [options]');
+}
