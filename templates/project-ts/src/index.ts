9785f9fbdb17c2f6d8603cca5dd0077cfa4d0c66
Martin Minkov <minkovlmartin@gmail.com>
2021-12-07 13:27:40 -0800

    feat: Add example contract that adds 1 + 2

--- templates/project-ts/src/index.ts
+++ templates/project-ts/src/index.ts
@@ -1,4 +1,37 @@
-// import * as SnarkyJS from 'snarkyjs';
-export function foo(): number {
-  return 1;
+import {
+  Field,
+  PublicKey,
+  SmartContract,
+  state,
+  State,
+  method,
+  UInt64,
+} from 'snarkyjs';
+
+/**
+ * Basic Example
+ * See https://docs.minaprotocol.com/snapps for more info.
+ *
+ * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
+ * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
+ */
+class Add extends SmartContract {
+  @state(Field) num: State<Field>;
+
+  constructor(
+    initialBalance: UInt64,
+    address: PublicKey,
+    num: Field = Field(1)
+  ) {
+    super(address);
+    this.balance.addInPlace(initialBalance);
+    this.num = State.init(num);
+  }
+
+  @method async update() {
+    const currentState = await this.num.get();
+    const newState = currentState.add(2);
+    newState.assertEquals(currentState.add(2));
+    this.num.set(newState);
+  }
 }
