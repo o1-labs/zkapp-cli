25514d4f777ea229368bf8e53ed20e38f1b33e87
Martin Minkov <minkovlmartin@gmail.com>
2021-12-08 15:32:17 -0800

    fix: Make jest test run successfully

--- templates/project-ts/src/index.ts
+++ templates/project-ts/src/index.ts
@@ -15,7 +15,7 @@ import {
  * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
  * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
  */
-class Add extends SmartContract {
+export default class Add extends SmartContract {
   @state(Field) num: State<Field>;
 
   constructor(
