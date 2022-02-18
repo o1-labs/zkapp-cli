ea589b46a5376f46d9fb56ad3c6b5daa27fbfcdb
Jason <jasongitmail@gmail.com>
2022-02-18 17:33:38 +0000

    feat: add config command

---
+++ src/lib/config.js
@@ -0,0 +1,156 @@
+const fs = require('fs-extra');
+const findPrefix = require('find-npm-prefix');
+const { prompt } = require('enquirer');
+const { table, getBorderCharacters } = require('table');
+const { step } = require('./helpers');
+const { green, red, bold, gray, reset } = require('chalk');
+
+const log = console.log;
+
+/**
+ * Show existing networks in `config.json` and allow a user to add a new
+ * network and url--and generate a key pair for it.
+ * @returns {void}
+ */
+async function config() {
+  // Get project root, so the CLI command can be run anywhere inside their proj.
+  const DIR = await findPrefix(process.cwd());
+
+  let config;
+  try {
+    config = fs.readJSONSync(`${DIR}/config.json`);
+  } catch (err) {
+    let str;
+    if (err.code === 'ENOENT') {
+      str = `config.json not found. Make sure you're in a snapp project.`;
+    } else {
+      str = 'Unable to read config.json.';
+      console.error(err);
+    }
+    log(red(str));
+    return;
+  }
+
+  // Build table of existing networks found in their config.json
+  let tableData = [[bold('Name'), bold('Url'), bold('Smart Contract')]];
+  for (const network in config.networks) {
+    const { url, smartContract } = config.networks[network];
+    tableData.push([
+      network,
+      url ?? '',
+      smartContract ?? gray('(never deployed)'),
+    ]);
+  }
+
+  // Sort alphabetically by network name.
+  tableData = tableData.sort((a, b) => a[0].localeCompare(b[0]));
+
+  const tableConfig = {
+    border: getBorderCharacters('norc'),
+    header: {
+      alignment: 'center',
+      content: bold('Networks in config.json'),
+    },
+  };
+
+  // Show "none found", if no networks exist.
+  if (tableData.length === 1) {
+    // Add some padding to empty name & url columns, to feel more natural.
+    tableData[0][0] = tableData[0][0] + ' '.repeat(2);
+    tableData[0][1] = tableData[0][1] + ' '.repeat(3);
+
+    tableData.push([[gray('None found')], [], []]);
+    tableConfig.spanningCells = [{ col: 0, row: 1, colSpan: 3 }];
+  }
+
+  // Print the table. Indented by 2 spaces for alignment in terminal.
+  const msg = '\n  ' + table(tableData, tableConfig).replaceAll('\n', '\n  ');
+  log(msg);
+
+  console.log('Add a new network:');
+
+  // TODO: Later, show pre-configured list to choose from or let user
+  // add a custom network.
+
+  function formatPrefixSymbol(state) {
+    // Shows a cyan question mark when not submitted.
+    // Shows a green check mark when submitted.
+    // Shows a red "x" if ctrl+C is pressed.
+
+    // Can't override the validating prefix or styling unfortunately
+    // https://github.com/enquirer/enquirer/blob/8d626c206733420637660ac7c2098d7de45e8590/lib/prompt.js#L125
+    // if (state.validating) return ''; // use no symbol, instead of pointer
+
+    if (!state.submitted) return state.symbols.question;
+    return state.cancelled ? red(state.symbols.cross) : state.symbols.check;
+  }
+
+  const response = await prompt([
+    {
+      type: 'input',
+      name: 'network',
+      message: (state) => {
+        const style = state.submitted && !state.cancelled ? green : reset;
+        return style('Choose a name (can be anything):');
+      },
+      prefix: formatPrefixSymbol,
+      validate: async (val) => {
+        val = val.toLowerCase().trim().replace(' ', '-');
+        if (!val) return red('Name is required.');
+        if (Object.keys(config.networks).includes(val)) {
+          return red('Name already exists.');
+        }
+        return true;
+      },
+      result: (val) => val.toLowerCase().trim().replace(' ', '-'),
+    },
+    {
+      type: 'input',
+      name: 'url',
+      message: (state) => {
+        const style = state.submitted && !state.cancelled ? green : reset;
+        return style('Set URL to deploy to:');
+      },
+      prefix: formatPrefixSymbol,
+      validate: (val) => {
+        if (!val) return red('Url is required.');
+        return true;
+      },
+      result: (val) => val.trim().replace(/ /, ''),
+    },
+  ]);
+
+  // If user presses "ctrl + c" during interactive prompt, exit.
+  const { network, url } = response;
+  if (!network || !url) return;
+
+  let keyPair = {};
+  await step(`Create key pair at keys/${network}.json`, async () => {
+    // TODO: BLOCKED Generate public/private key pair.
+    keyPair = {
+      publicKey: 'temp-abc123',
+      privateKey: 'temp-xyz987',
+    };
+
+    fs.outputJsonSync(`${DIR}/keys/${network}.json`, keyPair, { spaces: 2 });
+  });
+
+  await step(`Add network to config.json`, async () => {
+    config.networks[network] = { url, keyPath: `keys/${network}.json` };
+    fs.outputJsonSync(`${DIR}/config.json`, config, { spaces: 2 });
+  });
+
+  const str =
+    `\nSuccess!\n` +
+    `\nNext steps:` +
+    `\n  - If this is a testnet, request tMINA at:\n    https://faucet.minaprotocol.com?address=${encodeURIComponent(
+      keyPair.publicKey
+    )}` +
+    `\n  - To deploy, run: \`snapp deploy ${network}\``;
+
+  log(green(str));
+}
+
+module.exports = {
+  config,
+};
