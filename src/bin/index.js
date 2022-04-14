dc793851f83a25201ffa514d406ca1f629a01950
Martin Minkov <minkovlmartin@gmail.com>
2022-04-14 11:06:21 -0700

    feat: Refactor for wasm workers and use specified zkapp in keys dir

--- src/bin/index.js
+++ src/bin/index.js
@@ -4,9 +4,8 @@ const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const { project } = require('../lib/project');
 const { file } = require('../lib/file');
-// const { config } = require('../lib/config');
-// const { deploy } = require('../lib/deploy');
-// const { isReady, shutdown } = require('snarkyjs');
+const { config } = require('../lib/config');
+const { deploy } = require('../lib/deploy');
 const { example } = require('../lib/example');
 const { system } = require('../lib/system');
 const chalk = require('chalk');
@@ -15,76 +14,77 @@ const _g = chalk.green;
 const _r = chalk.reset;
 const _red = chalk.red;
 
-yargs(hideBin(process.argv))
-  .scriptName(_g('zk'))
-  .usage('Usage: $0 <command> [options]')
-  .strictCommands()
-  .strictOptions()
+(async () => {
+  const { isReady, shutdown } = await import('snarkyjs');
+  await isReady;
+  yargs(hideBin(process.argv))
+    .scriptName(_g('zk'))
+    .usage('Usage: $0 <command> [options]')
+    .strictCommands()
+    .strictOptions()
 
-  // https://github.com/yargs/yargs/issues/199
-  // https://github.com/yargs/yargs/blob/master/locales/en.json
-  .updateStrings({
-    'Missing required argument: %s': {
-      one: _red('Missing required argument: %s'),
-    },
-    'Unknown argument: %s': {
-      one: _red('Unknown argument: %s'),
-    },
-  })
-  .demandCommand(1, _red('Please provide a command.'))
+    // https://github.com/yargs/yargs/issues/199
+    // https://github.com/yargs/yargs/blob/master/locales/en.json
+    .updateStrings({
+      'Missing required argument: %s': {
+        one: _red('Missing required argument: %s'),
+      },
+      'Unknown argument: %s': {
+        one: _red('Unknown argument: %s'),
+      },
+    })
+    .demandCommand(1, _red('Please provide a command.'))
 
-  .command(
-    ['project [name]', 'proj [name]', 'p [name]'],
-    'Create a new project',
-    { name: { demand: true, string: true, hidden: true } },
-    async (argv) => await project(argv.name)
-  )
-  .command(
-    ['file [name]', 'f [name]'],
-    'Create a new file & test',
-    { name: { demand: true, string: true, hidden: true } },
-    (argv) => file(argv.name)
-  )
-  // .command(['config'], 'Add a new network & key pair', {}, config)
-  // .command(
-  //   ['deploy [network]'],
-  //   'Deploy or redeploy a zkApp',
-  //   {
-  //     network: { demand: true, string: true, hidden: true },
-  //     y: {
-  //       alias: 'yes',
-  //       boolean: true,
-  //       demand: false,
-  //       hidden: false,
-  //       description:
-  //         'Respond `yes` to all confirmation prompts.\nAllows running non-interactively within a script.',
-  //     },
-  //   },
-  //   async (argv) => {
-  //     await isReady;
-  //     deploy(argv);
-  //     await shutdown();
-  //   }
-  // )
-  .command(
-    ['example [name]', 'e [name]'],
-    'Create an example project',
-    { name: { demand: true, string: true, hidden: true } },
-    async (argv) => await example(argv.name)
-  )
-  .command(['system', 'sys', 's'], 'Show system info', {}, () => system())
-  .alias('h', 'help')
-  .alias('v', 'version')
+    .command(
+      ['project [name]', 'proj [name]', 'p [name]'],
+      'Create a new project',
+      { name: { demand: true, string: true, hidden: true } },
+      async (argv) => await project(argv.name)
+    )
+    .command(
+      ['file [name]', 'f [name]'],
+      'Create a new file & test',
+      { name: { demand: true, string: true, hidden: true } },
+      (argv) => file(argv.name)
+    )
+    .command(['config'], 'Add a new network & key pair', {}, config)
+    .command(
+      ['deploy [network]'],
+      'Deploy or redeploy a zkApp',
+      {
+        network: { demand: true, string: true, hidden: true },
+        y: {
+          alias: 'yes',
+          boolean: true,
+          demand: false,
+          hidden: false,
+          description:
+            'Respond `yes` to all confirmation prompts.\nAllows running non-interactively within a script.',
+        },
+      },
+      async (argv) => await deploy(argv)
+    )
+    .command(
+      ['example [name]', 'e [name]'],
+      'Create an example project',
+      { name: { demand: true, string: true, hidden: true } },
+      async (argv) => await example(argv.name)
+    )
+    .command(['system', 'sys', 's'], 'Show system info', {}, () => system())
+    .alias('h', 'help')
+    .alias('v', 'version')
 
-  .epilog(
-    // _r is a hack to force the terminal to retain a line break below
-    _r(
-      `
+    .epilog(
+      // _r is a hack to force the terminal to retain a line break below
+      _r(
+        `
 
     █▄ ▄█ █ █▄ █ ▄▀▄
     █ ▀ █ █ █ ▀█ █▀█
 
      Mina Protocol
       `
-    )
-  ).argv;
+      )
+    ).argv;
+  await shutdown();
+})();
