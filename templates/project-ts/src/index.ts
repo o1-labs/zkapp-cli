9af56f9fc914663757a5f6698e48fae7af772b44
Martin Minkov <minkovlmartin@gmail.com>
2022-05-04 10:42:53 -0700

    fix: Switch to using named export instead of default export

--- templates/project-ts/src/index.ts
+++ templates/project-ts/src/index.ts
@@ -7,7 +7,7 @@ import { Field, SmartContract, state, State, method } from 'snarkyjs';
  * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
  * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
  */
-export default class Add extends SmartContract {
+export class Add extends SmartContract {
   @state(Field) num = State<Field>();
 
   // initialization
