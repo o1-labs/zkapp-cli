7142683f98d3dc4d460382881cb5189039e0bb1a
Martin Minkov <minkovlmartin@gmail.com>
2022-05-06 13:11:44 -0700

    feat: Finalize deploy workflow

--- src/bin/index.js
+++ src/bin/index.js
@@ -14,77 +14,72 @@ const _g = chalk.green;
 const _r = chalk.reset;
 const _red = chalk.red;
 
-(async () => {
-  const { isReady, shutdown } = await import('snarkyjs');
-  await isReady;
-  yargs(hideBin(process.argv))
-    .scriptName(_g('zk'))
-    .usage('Usage: $0 <command> [options]')
-    .strictCommands()
-    .strictOptions()
+yargs(hideBin(process.argv))
+  .scriptName(_g('zk'))
+  .usage('Usage: $0 <command> [options]')
+  .strictCommands()
+  .strictOptions()
 
-    // https://github.com/yargs/yargs/issues/199
-    // https://github.com/yargs/yargs/blob/master/locales/en.json
-    .updateStrings({
-      'Missing required argument: %s': {
-        one: _red('Missing required argument: %s'),
-      },
-      'Unknown argument: %s': {
-        one: _red('Unknown argument: %s'),
-      },
-    })
-    .demandCommand(1, _red('Please provide a command.'))
+  // https://github.com/yargs/yargs/issues/199
+  // https://github.com/yargs/yargs/blob/master/locales/en.json
+  .updateStrings({
+    'Missing required argument: %s': {
+      one: _red('Missing required argument: %s'),
+    },
+    'Unknown argument: %s': {
+      one: _red('Unknown argument: %s'),
+    },
+  })
+  .demandCommand(1, _red('Please provide a command.'))
 
-    .command(
-      ['project [name]', 'proj [name]', 'p [name]'],
-      'Create a new project',
-      { name: { demand: true, string: true, hidden: true } },
-      async (argv) => await project(argv.name)
-    )
-    .command(
-      ['file [name]', 'f [name]'],
-      'Create a new file & test',
-      { name: { demand: true, string: true, hidden: true } },
-      (argv) => file(argv.name)
-    )
-    .command(['config'], 'Add a new network & key pair', {}, config)
-    .command(
-      ['deploy [network]'],
-      'Deploy or redeploy a zkApp',
-      {
-        network: { demand: true, string: true, hidden: true },
-        y: {
-          alias: 'yes',
-          boolean: true,
-          demand: false,
-          hidden: false,
-          description:
-            'Respond `yes` to all confirmation prompts.\nAllows running non-interactively within a script.',
-        },
+  .command(
+    ['project [name]', 'proj [name]', 'p [name]'],
+    'Create a new project',
+    { name: { demand: true, string: true, hidden: true } },
+    async (argv) => await project(argv.name)
+  )
+  .command(
+    ['file [name]', 'f [name]'],
+    'Create a new file & test',
+    { name: { demand: true, string: true, hidden: true } },
+    (argv) => file(argv.name)
+  )
+  .command(['config'], 'Add a new network & key pair', {}, config)
+  .command(
+    ['deploy [network]'],
+    'Deploy or redeploy a zkApp',
+    {
+      network: { demand: true, string: true, hidden: true },
+      y: {
+        alias: 'yes',
+        boolean: true,
+        demand: false,
+        hidden: false,
+        description:
+          'Respond `yes` to all confirmation prompts.\nAllows running non-interactively within a script.',
       },
-      async (argv) => await deploy(argv)
-    )
-    .command(
-      ['example [name]', 'e [name]'],
-      'Create an example project',
-      { name: { demand: true, string: true, hidden: true } },
-      async (argv) => await example(argv.name)
-    )
-    .command(['system', 'sys', 's'], 'Show system info', {}, () => system())
-    .alias('h', 'help')
-    .alias('v', 'version')
+    },
+    async (argv) => await deploy(argv)
+  )
+  .command(
+    ['example [name]', 'e [name]'],
+    'Create an example project',
+    { name: { demand: true, string: true, hidden: true } },
+    async (argv) => await example(argv.name)
+  )
+  .command(['system', 'sys', 's'], 'Show system info', {}, () => system())
+  .alias('h', 'help')
+  .alias('v', 'version')
 
-    .epilog(
-      // _r is a hack to force the terminal to retain a line break below
-      _r(
-        `
+  .epilog(
+    // _r is a hack to force the terminal to retain a line break below
+    _r(
+      `
 
     █▄ ▄█ █ █▄ █ ▄▀▄
     █ ▀ █ █ █ ▀█ █▀█
 
      Mina Protocol
       `
-      )
-    ).argv;
-  await shutdown();
-})();
+    )
+  ).argv;
