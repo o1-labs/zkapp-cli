ff736aef23aed41b715357beafcc03712b7de609
Martin Minkov <minkovlmartin@gmail.com>
2022-05-25 09:49:12 -0700

    feat(ci): Replace runTests.sh with ci.yml

--- templates/project-ts/src/index.test.ts
+++ templates/project-ts/src/index.test.ts
@@ -6,7 +6,12 @@ describe('index.ts', () => {
       await isReady;
     });
     afterAll(async () => {
-      await shutdown();
+      // `shutdown()` internally calls `process.exit()` which will exit the running Jest process early.
+      // Specifying a timeout of 0 is a workaround to defer `shutdown()` until Jest is done running all tests.
+      // This should be fixed with https://github.com/MinaProtocol/mina/issues/10943
+      setTimeout(async () => {
+        await shutdown();
+      }, 0);
     });
     it('should be correct', async () => {
       expect(Field(1).add(1)).toEqual(Field(2));
