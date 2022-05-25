b8c5a3af4a1729784d69b508a475097ea30b3a90
ymekuria <ymekuria@gmail.com>
2022-05-25 13:02:43 -0700

    rename contract

--- templates/project-ts/src/index.ts
+++ templates/project-ts/src/index.ts
@@ -11,10 +11,10 @@ import {
  * Basic Example
  * See https://docs.minaprotocol.com/zkapps for more info.
  *
- * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
+ * The TestCache contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
  * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
  */
-export class TestCache extends SmartContract {
+export class Fix extends SmartContract {
   @state(Field) num = State<Field>();
 
   // initialization
