3bf8fae3aff0ec4f262e8c5b05af75026cf51c6a
Martin Minkov <minkovlmartin@gmail.com>
2022-05-31 14:23:24 -0700

    feat(tictactoe): Add lib and test file

--- examples/tictactoe/ts/src/index.ts
+++ examples/tictactoe/ts/src/index.ts
@@ -10,40 +10,26 @@
  * Run with node:     `$ node build/src/index.js`.
  */
 
-import {
-  Field,
-  PrivateKey,
-  Mina,
-  Party,
-  shutdown,
-  Signature,
-  isReady,
-} from 'snarkyjs';
+import { Field, PrivateKey, Mina, shutdown, isReady } from 'snarkyjs';
+import { createLocalBlockchain, makeMove, deploy } from './tictactoe-lib.js';
 import { TicTacToe, Board } from './tictactoe.js';
 
-async function main() {
+async function playTicTacToe() {
   await isReady;
 
-  const Local = Mina.LocalBlockchain();
-  Mina.setActiveInstance(Local);
-
-  const player1 = Local.testAccounts[0].privateKey;
-  const player2 = Local.testAccounts[1].privateKey;
+  const accounts = createLocalBlockchain();
+  const player1 = accounts[0];
+  const player2 = accounts[1];
   const player1Public = player1.toPublicKey();
   const player2Public = player2.toPublicKey();
 
   const zkAppPrivkey = PrivateKey.random();
   const zkAppPubkey = zkAppPrivkey.toPublicKey();
+  let zkAppInstance = new TicTacToe(zkAppPubkey);
 
   // Create a new instance of the contract
   console.log('\n\n====== DEPLOYING ======\n\n');
-  let zkAppInstance = new TicTacToe(zkAppPubkey);
-
-  let txn = await Local.transaction(player1, () => {
-    Party.fundNewAccount(player1);
-    zkAppInstance.deploy({ zkappKey: zkAppPrivkey });
-  });
-  await txn.send().wait();
+  await deploy(zkAppInstance, zkAppPrivkey, player1);
 
   console.log('after transaction');
 
@@ -59,21 +45,15 @@ async function main() {
 
   // play
   console.log('\n\n====== FIRST MOVE ======\n\n');
-  txn = await Local.transaction(player1, async () => {
-    const x = Field.zero;
-    const y = Field.zero;
-    const signature = Signature.create(player1, [x, y]);
-    zkAppInstance.play(
-      player1Public,
-      signature,
-      Field.zero,
-      Field.zero,
-      player1Public,
-      player2Public
-    );
-    zkAppInstance.sign(zkAppPrivkey);
-  });
-  txn.send().wait();
+  await makeMove(
+    zkAppInstance,
+    zkAppPrivkey,
+    player1,
+    player1Public,
+    player2Public,
+    Field.zero,
+    Field.zero
+  );
 
   // debug
   b = await Mina.getAccount(zkAppPubkey);
@@ -81,66 +61,45 @@ async function main() {
 
   // play
   console.log('\n\n====== SECOND MOVE ======\n\n');
-  txn = await Local.transaction(player1, async () => {
-    const x = Field.one;
-    const y = Field.zero;
-    const signature = Signature.create(player2, [x, y]);
-    zkAppInstance.play(
-      player2Public,
-      signature,
-      Field.one,
-      Field.zero,
-      player1Public,
-      player2Public
-    );
-    zkAppInstance.sign(zkAppPrivkey);
-  });
-  txn.send().wait();
-
+  await makeMove(
+    zkAppInstance,
+    zkAppPrivkey,
+    player2,
+    player1Public,
+    player2Public,
+    Field.one,
+    Field.zero
+  );
   // debug
   b = await Mina.getAccount(zkAppPubkey);
   new Board(b.zkapp?.appState?.[0]!).printState();
 
   // play
   console.log('\n\n====== THIRD MOVE ======\n\n');
-  txn = await Local.transaction(player1, async () => {
-    const x = Field.one;
-    const y = Field.one;
-    const signature = Signature.create(player1, [x, y]);
-    zkAppInstance.play(
-      player1Public,
-      signature,
-      Field.one,
-      Field.one,
-      player1Public,
-      player2Public
-    );
-    zkAppInstance.sign(zkAppPrivkey);
-  });
-  txn.send().wait();
-
+  await makeMove(
+    zkAppInstance,
+    zkAppPrivkey,
+    player1,
+    player1Public,
+    player2Public,
+    Field.one,
+    Field.one
+  );
   // debug
   b = await Mina.getAccount(zkAppPubkey);
   new Board(b.zkapp?.appState?.[0]!).printState();
 
   // play
   console.log('\n\n====== FOURTH MOVE ======\n\n');
-  const two = new Field(2);
-  txn = await Local.transaction(player2, async () => {
-    const x = two;
-    const y = Field.one;
-    const signature = Signature.create(player2, [x, y]);
-    zkAppInstance.play(
-      player2Public,
-      signature,
-      two,
-      Field.one,
-      player1Public,
-      player2Public
-    );
-    zkAppInstance.sign(zkAppPrivkey);
-  });
-  await txn.send().wait();
+  await makeMove(
+    zkAppInstance,
+    zkAppPrivkey,
+    player2,
+    player1Public,
+    player2Public,
+    Field(2),
+    Field.one
+  );
 
   // debug
   b = await Mina.getAccount(zkAppPubkey);
@@ -148,21 +107,15 @@ async function main() {
 
   // play
   console.log('\n\n====== FIFTH MOVE ======\n\n');
-  txn = await Local.transaction(player1, async () => {
-    const x = two;
-    const y = two;
-    const signature = Signature.create(player1, [x, y]);
-    zkAppInstance.play(
-      player1Public,
-      signature,
-      two,
-      two,
-      player1Public,
-      player2Public
-    );
-    zkAppInstance.sign(zkAppPrivkey);
-  });
-  await txn.send().wait();
+  await makeMove(
+    zkAppInstance,
+    zkAppPrivkey,
+    player1,
+    player1Public,
+    player2Public,
+    Field(2),
+    Field(2)
+  );
 
   // debug
   b = await Mina.getAccount(zkAppPubkey);
@@ -176,4 +129,4 @@ async function main() {
   await shutdown();
 }
 
-main();
+playTicTacToe();
