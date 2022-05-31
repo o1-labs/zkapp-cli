f1bfee1615f10d156b7d0163dc8452f44e7b24d2
ymekuria <ymekuria@gmail.com>
2022-05-31 10:50:10 -0700

    fix: fix typo

--- examples/sudoku/ts/src/index.ts
+++ examples/sudoku/ts/src/index.ts
@@ -1,6 +1,6 @@
 /**
  * This file specifies how to run the `SudokuZkApp` smart contract locally using the `Mina.LocalBlockchain()` method.
- * The `Mina.LocalBlockchain()` method specifes a ledger of accounts and contains logic for updating the ledger.
+ * The `Mina.LocalBlockchain()` method specifies a ledger of accounts and contains logic for updating the ledger.
  *
  * Please note that this deployment is local and does not deploy to a live network.
  * If you wish to deploy to a live network, please use the zkapp-cli to deploy.
