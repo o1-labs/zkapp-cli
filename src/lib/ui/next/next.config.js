fc7d364a047420a9f0b437309410d8fbcc81b373
Martin Minkov <minkovlmartin@gmail.com>
2022-10-20 15:56:52 -0700

    Add comments to direct to docs for info on headers

--- src/lib/ui/next/next.config.js
+++ src/lib/ui/next/next.config.js
@@ -1,3 +1,5 @@
+// To properly enable SnarkyJS for the web, we must set the COOP and COEP headers.
+// See here for more information: https://docs.minaprotocol.com/zkapps/how-to-write-a-zkapp-ui#enabling-coop-and-coep-headers
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
