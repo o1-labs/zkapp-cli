716a83e05376b858a4d397f49c4d324988fe6ead
Martin Minkov <minkovlmartin@gmail.com>
2022-10-20 21:24:57 -0700

    Update comments text

--- src/lib/ui/svelte/hooks.server.js
+++ src/lib/ui/svelte/hooks.server.js
@@ -1,4 +1,4 @@
-// To properly enable SnarkyJS for the web, we must set the COOP and COEP headers.
+// To enable SnarkyJS for the web, we must set the COOP and COEP headers.
 // See here for more information: https://docs.minaprotocol.com/zkapps/how-to-write-a-zkapp-ui#enabling-coop-and-coep-headers
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
