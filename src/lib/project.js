37ddaf338410fe54d3a491945c6052da278c5d49
Martin Minkov <minkovlmartin@gmail.com>
2022-10-20 21:24:57 -0700

    Update comments text

--- src/lib/project.js
+++ src/lib/project.js
@@ -297,6 +297,8 @@ function scaffoldNext() {
   const newNextConfig = nextConfig.replace(
     /^}(.*?)$/gm, // Search for the last '}' in the file.
     `
+  // To enable SnarkyJS for the web, we must set the COOP and COEP headers.
+  // See here for more information: https://docs.minaprotocol.com/zkapps/how-to-write-a-zkapp-ui#enabling-coop-and-coep-headers
   async headers() {
     return [
       {
