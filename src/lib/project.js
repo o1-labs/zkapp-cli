4a3664e2cefaabbaf5c2656c408ad6102ab96754
Evan Shapiro <evan@minaprotocol.com>
2022-10-29 15:33:05 -0700

    fix an import

--- src/lib/project.js
+++ src/lib/project.js
@@ -463,7 +463,7 @@ async function scaffoldNext(projectName) {
     );
     apptsx = apptsx.replace(
       'export default function',
-      `import { loadCOISerivceWorker } from './reactCOIServiceWorker';
+      `import './reactCOIServiceWorker';
 
 export default function`
     );
