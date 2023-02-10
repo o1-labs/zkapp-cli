715c06c3dee5d03bdfb962e76b1090d7534721e0
ymekuria <ymekuria@gmail.com>
2023-02-10 14:23:54 +0900

    refactor: remove redundant code

--- src/lib/config.js
+++ src/lib/config.js
@@ -34,8 +34,9 @@ async function config() {
 
   // Checks if developer has the legacy networks in config.json and renames it to deploy aliases.
   if (Object.prototype.hasOwnProperty.call(config, 'networks')) {
-    Object.assign(config, { deployAliases: config.networks })['networks'];
-    delete config['networks'];
+    delete Object.assign(config, { deployAliases: config.networks })[
+      'networks'
+    ];
   }
 
   // Build table of existing deploy aliases found in their config.json
