331fd7e7ff5a89a79ee1d8d9d620d9053bd6dc98
Test <test@example.com>
2023-03-24 02:36:13 -0600

    feat: landing page mina logo and tagline

--- src/lib/ui/nuxt/customNuxtIndex.js
+++ src/lib/ui/nuxt/customNuxtIndex.js
@@ -1,1 +1,20 @@
-module.exports = ``;
+module.exports = `
+<template>
+  <main class="main">
+    <div class="center">
+      <img
+        class="logo"
+        src="~/assets/HeroMinaLogo_gray.svg"
+        alt="Mina Logo"
+        width="191"
+        height="174"
+        priority
+      />
+      <p class="tagline">
+        Built with&nbsp;
+        <code class="code">SnarkyJS</code>
+      </p>
+    </div>
+  </main>
+</template>
+`;
