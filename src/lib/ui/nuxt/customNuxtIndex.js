c0f15d1bbd4fd8a9fcaf4be966563d3508d9a93f
Test <test@example.com>
2023-03-24 02:47:13 -0600

    feat: add get started section

--- src/lib/ui/nuxt/customNuxtIndex.js
+++ src/lib/ui/nuxt/customNuxtIndex.js
@@ -15,6 +15,10 @@ module.exports = `
         <code class="code">SnarkyJS</code>
       </p>
     </div>
+    <p class="start">
+      Get started by editing&nbsp;
+      <code class="code">pages/index.vue</code>
+    </p>    
   </main>
 </template>
 `;
