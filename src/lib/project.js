b107e47d4cccfcfa655dafe913066a82b24f7a81
Test <test@example.com>
2023-03-27 04:40:42 -0600

    refactor: remove unused code

--- src/lib/project.js
+++ src/lib/project.js
@@ -689,35 +689,6 @@ function scaffoldNuxt() {
   pkgJson.devDependencies['nuxt-vite'] = '0.*';
   fs.writeJSONSync(path.join('ui', 'package.json'), pkgJson, { spaces: 2 });
 
-  //   const customNuxtIndex = `
-  // <template>
-  // <Tutorial />
-  // </template>
-
-  // <script>
-  // import { isReady, Mina, PublicKey } from 'snarkyjs'
-
-  // export default {
-  //   async mounted() {
-  //     await isReady
-
-  //     const { Add } = await import('../../contracts/build/src/')
-  //     // Update this to use the address (public key) for your zkApp account
-  //     // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
-  //     // Berkeley Testnet B62qisn669bZqsh8yMWkNyCA7RvjrL6gfdr3TQxymDHNhTc97xE5kNV
-  //     const zkAppAddress = ''
-  //     // This should be removed once the zkAppAddress is updated.
-  //     if (!zkAppAddress) {
-  //       console.error(
-  //         'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qqkb7hD1We6gEfrcqosKt9C398VLp1WXeTo1i9boPoqF7B1LxHg4',
-  //       )
-  //     }
-  //     const zkApp = new Add(PublicKey.fromBase58(zkAppAddress))
-  //   }
-  // }
-  // </script>
-  // `;
-  // Adds landing page to Nuxt project.
   fs.writeFileSync(path.join('ui', 'pages', 'index.vue'), customNuxtIndex);
 
   // Adds landing page assets directory and files to Nuxt project.
