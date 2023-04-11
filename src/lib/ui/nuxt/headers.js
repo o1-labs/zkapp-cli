3554c286a5c2fcdbef5e485a29abb6ed54df82a9
ymekuria <ymekuria@gmail.com>
2023-04-11 01:07:09 -0600

    feat: update coop an coep headers middleware for nuxt 3

--- src/lib/ui/nuxt/headers.js
+++ src/lib/ui/nuxt/headers.js
@@ -1,7 +1,6 @@
 // To enable SnarkyJS for the web, we must set the COOP and COEP headers.
 // See here for more information: https://docs.minaprotocol.com/zkapps/how-to-write-a-zkapp-ui#enabling-coop-and-coep-headers
-export default function (req, res, next) {
+export default defineEventHandler(({ node: { req, res } }) => {
   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  next();
-}
+});
