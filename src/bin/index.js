0e739e355403d29eed5bf338f5d0a373b254e532
Barrie Byron <barrie@o1labs.org>
2023-04-27 15:29:46 -0400

    fix: update file CLI help text (#392)

--- src/bin/index.js
+++ src/bin/index.js
@@ -56,7 +56,7 @@ yargs(hideBin(process.argv))
   )
   .command(
     ['file [name]', 'f [name]'],
-    'Create a new file & test',
+    'Create a file and generate the corresponding test file',
     { name: { demand: true, string: true, hidden: true } },
     (argv) => file(argv.name)
   )
