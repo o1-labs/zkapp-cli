d48be7abe18e86697a4537a69687a5089ccc1a11
ymekuria <ymekuria@gmail.com>
2023-05-02 22:45:02 -0700

    feat: remove isready and shutdown from next landing page

--- src/lib/ui/next/customNextIndex.js
+++ src/lib/ui/next/customNextIndex.js
@@ -8,10 +8,8 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   useEffect(() => {
     (async () => {
-      const { Mina, isReady, PublicKey } = await import('snarkyjs');
+      const { Mina, PublicKey } = await import('snarkyjs');
       const { Add } = await import('../../../contracts/build/src/');
-
-      await isReady;
 
       // Update this to use the address (public key) for your zkApp account.
       // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
