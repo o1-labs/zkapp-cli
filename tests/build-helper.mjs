8f390fc92bdb135d0cca58772e194826bf1e7488
Serhii Shymkiv <sergey@shimkiv.com>
2023-06-02 12:25:53 +0300

    The CLI testing lib workaround for Windows platform in order to set 'shell: true' for child process spawning.

---
+++ tests/build-helper.mjs
@@ -0,0 +1,17 @@
+import replace from 'replace-in-file';
+
+const options = {
+  files: './node_modules/@gmrchk/cli-testing-library/lib/createExecute.js',
+  from: /cwd: runFrom \? path_1.default.join\(base, runFrom\) : path_1\.default\.join\(base\),/g,
+  to: 'cwd: runFrom ? path_1.default.join(base, runFrom) : path_1.default.join(base),\n                shell: true',
+};
+
+try {
+  const os = process.platform;
+  if (os === 'win32' || os === 'win64') {
+    const results = await replace(options);
+    console.log('Replacement results:', results);
+  }
+} catch (error) {
+  console.error('Error occurred:', error);
+}
