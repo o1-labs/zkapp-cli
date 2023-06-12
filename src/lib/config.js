0e9712422cd3c6a3c8275a1acddecde364833c04
ymekuria <ymekuria@gmail.com>
2023-06-13 01:11:05 +0200

    refactor: move deploy alias prompts to seperate file

--- src/lib/config.js
+++ src/lib/config.js
@@ -6,6 +6,7 @@ const { table, getBorderCharacters } = require('table');
 const { step } = require('./helpers');
 const { green, red, bold, gray, reset } = require('chalk');
 const Client = require('mina-signer');
+const { prompts } = require('./prompts');
 
 const log = console.log;
 
@@ -117,57 +118,6 @@ async function config() {
     return state.cancelled ? red(state.symbols.cross) : state.symbols.check;
   }
 
-  const deployAliasPrompts = [
-    {
-      type: 'input',
-      name: 'deployAliasName',
-      message: (state) => {
-        const style = state.submitted && !state.cancelled ? green : reset;
-        return style('Choose a name (can be anything):');
-      },
-      prefix: formatPrefixSymbol,
-      validate: async (val) => {
-        val = val.toLowerCase().trim().replace(' ', '-');
-        if (!val) return red('Name is required.');
-        if (Object.keys(config.deployAliases).includes(val)) {
-          return red('Name already exists.');
-        }
-        return true;
-      },
-      result: (val) => val.toLowerCase().trim().replace(' ', '-'),
-    },
-    {
-      type: 'input',
-      name: 'url',
-      message: (state) => {
-        const style = state.submitted && !state.cancelled ? green : reset;
-        return style('Set the Mina GraphQL API URL to deploy to:');
-      },
-      prefix: formatPrefixSymbol,
-      validate: (val) => {
-        if (!val) return red('Url is required.');
-        return true;
-      },
-      result: (val) => val.trim().replace(/ /, ''),
-    },
-    {
-      type: 'input',
-      name: 'fee',
-      message: (state) => {
-        const style = state.submitted && !state.cancelled ? green : reset;
-        return style('Set transaction fee to use when deploying (in MINA):');
-      },
-      prefix: formatPrefixSymbol,
-      validate: (val) => {
-        if (!val) return red('Fee is required.');
-        if (isNaN(val)) return red('Fee must be a number.');
-        if (val < 0) return red("Fee can't be negative.");
-        return true;
-      },
-      result: (val) => val.trim().replace(/ /, ''),
-    },
-  ];
-
   const initialFeepayerPrompts = [
     {
       type: 'select',
@@ -245,7 +195,7 @@ async function config() {
   ];
 
   const initialPromptResponse = await prompt([
-    ...deployAliasPrompts,
+    ...prompts.deployAliasPrompts(config),
     ...initialFeepayerPrompts,
   ]);
 
--- src/lib/prompts.js
+++ src/lib/prompts.js
@@ -1,6 +1,70 @@
 const { green, red, bold, gray, reset } = require('chalk');
 
-const prompts = {};
+function formatPrefixSymbol(state) {
+  // Shows a cyan question mark when not submitted.
+  // Shows a green check mark when submitted.
+  // Shows a red "x" if ctrl+C is pressed.
+
+  // Can't override the validating prefix or styling unfortunately
+  // https://github.com/enquirer/enquirer/blob/8d626c206733420637660ac7c2098d7de45e8590/lib/prompt.js#L125
+  // if (state.validating) return ''; // use no symbol, instead of pointer
+
+  if (!state.submitted) return state.symbols.question;
+  return state.cancelled ? red(state.symbols.cross) : state.symbols.check;
+}
+
+const prompts = {
+  deployAliasPrompts: (config) => [
+    {
+      type: 'input',
+      name: 'deployAliasName',
+      message: (state) => {
+        const style = state.submitted && !state.cancelled ? green : reset;
+        return style('Choose a name (can be anything):');
+      },
+      prefix: formatPrefixSymbol,
+      validate: async (val) => {
+        val = val.toLowerCase().trim().replace(' ', '-');
+        if (!val) return red('Name is required.');
+        if (Object.keys(config.deployAliases).includes(val)) {
+          return red('Name already exists.');
+        }
+        return true;
+      },
+      result: (val) => val.toLowerCase().trim().replace(' ', '-'),
+    },
+    {
+      type: 'input',
+      name: 'url',
+      message: (state) => {
+        const style = state.submitted && !state.cancelled ? green : reset;
+        return style('Set the Mina GraphQL API URL to deploy to:');
+      },
+      prefix: formatPrefixSymbol,
+      validate: (val) => {
+        if (!val) return red('Url is required.');
+        return true;
+      },
+      result: (val) => val.trim().replace(/ /, ''),
+    },
+    {
+      type: 'input',
+      name: 'fee',
+      message: (state) => {
+        const style = state.submitted && !state.cancelled ? green : reset;
+        return style('Set transaction fee to use when deploying (in MINA):');
+      },
+      prefix: formatPrefixSymbol,
+      validate: (val) => {
+        if (!val) return red('Fee is required.');
+        if (isNaN(val)) return red('Fee must be a number.');
+        if (val < 0) return red("Fee can't be negative.");
+        return true;
+      },
+      result: (val) => val.trim().replace(/ /, ''),
+    },
+  ],
+};
 module.exports = {
   prompts,
 };
