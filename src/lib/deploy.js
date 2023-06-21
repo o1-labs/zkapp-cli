23e67cb9f15ad14b2f7dbd1e2ecad0d68539cd9b
ymekuria <ymekuria@gmail.com>
2023-06-21 17:00:52 +0200

    feat: update user prompt to install latest cli and snarkyjs version

--- src/lib/deploy.js
+++ src/lib/deploy.js
@@ -50,7 +50,7 @@ async function deploy({ alias, yes }) {
   if (hasBreakingChanges(installedCliVersion, latestCliVersion)) {
     log(red(`You are using an old zkapp-cli version ${installedCliVersion}.`));
     log(red(`The current version is ${latestCliVersion}.`));
-    log(red('Run `npm update -g zkapp-cli && npm update snarkyjs`.'));
+    log(red('Run `npm update -g zkapp-cli && npm install snarkyjs@latest`.'));
     return;
   }
 
