456929db58c5b65362d98d1fe0e01af7c0a1b5b3
ymekuria <ymekuria@gmail.com>
2023-08-16 15:47:39 -0700

    chore(project.js): add explanation of basePath and assetPrefix for github deployment in next.config.js

--- src/lib/project.js
+++ src/lib/project.js
@@ -579,8 +579,16 @@ async function scaffoldNext(projectName) {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/${projectName}' : undefined, // update if your repo name changes for 'npm run deploy' to work successfully
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/${projectName}/' : undefined, // update if your repo name changes for 'npm run deploy' to work successfully
+
+  /* Used to serve the Next.js app from a subdirectory (the GitHub repo name) and 
+   * assetPrefix is used to serve assets (JS, CSS, images, etc.) from that subdirectory 
+   * when deployed to GitHub Pages. The assetPrefix needs to be added manually to any assets
+   * if they're not loaded by Next.js' automatic handling (for example, in CSS files or in a <img> element). 
+   * The 'ghp-postbuild.js' script in this project prepends the repo name to asset urls in the built css files 
+   * after runing 'npm run deploy'.
+   */
+  basePath: process.env.NODE_ENV === 'production' ? '/${projectName}' : '', // update if your repo name changes for 'npm run deploy' to work successfully
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/${projectName}/' : '', // update if your repo name changes for 'npm run deploy' to work successfully
 };`
     );
 
