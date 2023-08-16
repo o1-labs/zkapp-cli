a9dcb02b69a85a19a22919b3158890695ad80072
ymekuria <ymekuria@gmail.com>
2023-08-15 21:38:06 -0700

    refactor(ghp-postbuild.js): refactor post build script to use synchronous file operations for better readability and error handling

--- src/lib/ui/next/ghp-postbuild.js
+++ src/lib/ui/next/ghp-postbuild.js
@@ -4,32 +4,17 @@ const path = require('path');
 // This script modifies the built CSS files and prepends the repo-name to the asset URLs.
 // to be compatible with github pages deployment.
 const cssDir = path.join(__dirname, '/.next/static/css');
-// Add your repository name here.
+// Update your repository name here if it is different from the project name.
 let repoURL = '';
-fs.readdir(cssDir, (err, files) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
+const files = fs.readdirSync(cssDir);
+
+files.forEach((file) => {
+  if (path.extname(file) === '.css') {
+    const filePath = path.join(cssDir, file);
+
+    const data = fs.readFileSync(filePath, 'utf8');
+    const result = data.replace(/url\(\//g, `url(/${repoURL}/`);
+
+    fs.writeFileSync(filePath, result, 'utf8');
   }
-
-  files.forEach(file => {
-    if (path.extname(file) === '.css') {
-      const filePath = path.join(cssDir, file);
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-          process.exit(1);
-        }
-        
-        const result = data.replace(/url\(\//g, `url(/${repoURL}/`);
-
-        fs.writeFile(filePath, result, 'utf8', err => {
-          if (err) {
-            console.error(err);
-            process.exit(1);
-          }
-        });
-      });
-    }
-  });
 });
