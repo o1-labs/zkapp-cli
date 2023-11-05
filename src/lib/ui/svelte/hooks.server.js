e17d8d22a80b9c0ba5b2f664b8c000d86289fbab
Berzan <berzan@tutanota.com>
2023-11-05 14:48:55 +0000

    Fix TypeScript error while using SvelteKit.

--- src/lib/ui/svelte/hooks.server.js
+++
@@ -1,9 +0,0 @@
-// To enable o1js for the web, we must set the COOP and COEP headers.
-// See here for more information: https://docs.minaprotocol.com/zkapps/how-to-write-a-zkapp-ui#enabling-coop-and-coep-headers
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }) {
-  const response = await resolve(event);
-  response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
-  response.headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
-  return response;
-}
