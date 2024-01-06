7a6598ecbe557415fb2e67cf2fe9eb975b2e38cb
ymekuria <ymekuria@gmail.com>
2024-01-06 01:50:38 -0700

    feat(member.ts): add Member class for representing a member in a voting system
    The Member class represents a member in a voting system. It includes properties for the member's public key, balance, votes, and witnesses. The class also includes methods for getting the hash of the member, adding a vote, and creating an empty member instance.

---
+++ examples/voting/member.ts
@@ -0,0 +1,62 @@
+import {
+  Bool,
+  CircuitValue,
+  Field,
+  prop,
+  PublicKey,
+  UInt64,
+  Poseidon,
+  MerkleWitness,
+} from 'o1js';
+
+export class MyMerkleWitness extends MerkleWitness(3) {}
+let w = {
+  isLeft: false,
+  sibling: Field(0),
+};
+let dummyWitness = Array.from(Array(MyMerkleWitness.height - 1).keys()).map(
+  () => w
+);
+
+export class Member extends CircuitValue {
+  @prop publicKey: PublicKey;
+  @prop balance: UInt64;
+
+  // will need this to keep track of votes for candidates
+  @prop votes: Field;
+
+  @prop witness: MyMerkleWitness;
+  @prop votesWitness: MyMerkleWitness;
+
+  constructor(publicKey: PublicKey, balance: UInt64) {
+    super();
+    this.publicKey = publicKey;
+    this.balance = balance;
+    this.votes = Field(0);
+
+    this.witness = new MyMerkleWitness(dummyWitness);
+    this.votesWitness = new MyMerkleWitness(dummyWitness);
+  }
+
+  getHash(): Field {
+    return Poseidon.hash(
+      this.publicKey
+        .toFields()
+        .concat(this.balance.toFields())
+        .concat(this.votes.toFields())
+    );
+  }
+
+  addVote(): Member {
+    this.votes = this.votes.add(1);
+    return this;
+  }
+
+  static empty<T extends new (...args: any) => any>(): InstanceType<T> {
+    return new Member(PublicKey.empty(), UInt64.zero) as any;
+  }
+
+  static from(publicKey: PublicKey, balance: UInt64) {
+    return new Member(publicKey, balance);
+  }
+}
