252ffeda18ed03e0692fe0d6b4899d33df0459ad
ymekuria <ymekuria@gmail.com>
2024-02-15 02:35:19 -0800

    feat(deploy.js): add null check for smartContractImports before checking if contractName exists

--- src/lib/deploy.js
+++ src/lib/deploy.js
@@ -249,7 +249,7 @@ export async function deploy({ alias, yes }) {
   } catch (error) {
     // Attempt to import the smart contract class to deploy from the user's file.
     // If we cannot find the named export log an error message and return early.
-    if (!(contractName in smartContractImports)) {
+    if (smartContractImports && !(contractName in smartContractImports)) {
       log(
         chalk.red(
           `  Failed to find the "${contractName}" smart contract in your build directory.\n  Please confirm that your config.json contains the name of the smart contract that you want to deploy to this deploy alias\n and check that you have exported your smart contract class using a named export and try again.`
