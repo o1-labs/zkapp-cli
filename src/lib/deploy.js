a12fc6d15f5e6060afb49089d962d55342a3490c
ymekuria <ymekuria@gmail.com>
2024-03-06 14:59:55 -0700

    Revert "feat(deploy.js): explicitly import fetch from the Node.js built-in modules to ensure availablity regardless of environment"
    This reverts commit 73f21ecf0c2c6042f51ea4fff66ee80aaea4cf99.

--- src/lib/deploy.js
+++ src/lib/deploy.js
@@ -3,7 +3,6 @@ import { execSync } from 'child_process';
 import enquirer from 'enquirer';
 import glob from 'fast-glob';
 import findPrefix from 'find-npm-prefix';
-import fetch from 'node:fetch';
 import fs from 'fs-extra';
 import path from 'path';
 import { getBorderCharacters, table } from 'table';
