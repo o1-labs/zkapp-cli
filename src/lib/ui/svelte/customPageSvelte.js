d4b64c4bdfdece0d73f5a89ee4982cfb601af138
Serhii Shymkiv <sergey@shimkiv.com>
2024-03-26 12:08:06 +0200

    Reflect o1js deprecations removal. (#611)

--- src/lib/ui/svelte/customPageSvelte.js
+++ src/lib/ui/svelte/customPageSvelte.js
@@ -4,12 +4,11 @@ export default `
   import arrowRightSmall from '$lib/assets/arrow-right-small.svg'
   import GradientBG from './GradientBG.svelte'
   import { onMount } from 'svelte'
-  import { isReady, Mina, PublicKey } from 'o1js'
+  import { Mina, PublicKey } from 'o1js'
 
   onMount(async () => {
     const { Add } = await import('../../../contracts/build/src/')
 
-    await isReady
     // Update this to use the address (public key) for your zkApp account.
     // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
     // Berkeley Testnet B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA .
