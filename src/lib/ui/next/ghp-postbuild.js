c12b749b3f842a7d512c28c11dc64b07320647ae
Serhii Shymkiv <sergey@shimkiv.com>
2024-05-20 10:59:19 +0300

    Migrate to Next.js v14.2.3 and fix latest o1js usage.

--- src/lib/ui/next/ghp-postbuild.js
+++ src/lib/ui/next/ghp-postbuild.js
@@ -1,26 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import { dirname, extname, join } from 'path';
+import { fileURLToPath } from 'url';
 
-// This script modifies the built CSS files and prepends the repo-name to the asset URLs.
-// to be compatible with github pages deployment.
-const cssDir = path.join(__dirname, '/out/_next/static/css');
+// This script modifies the built CSS files and prepends the repo-name to the asset URLs
+// to be compatible with the GitHub pages deployments.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const cssDir = join(__dirname, '/out/_next/static/css');
 
 // Update your repository name here if it is different from the project name.
-let repoURL = '';
-const files = fs.readdirSync(cssDir);
+let repoURL = 'deploy-me';
+const files = readdirSync(cssDir);
 
 files.forEach((file) => {
-  if (path.extname(file) === '.css') {
-    const filePath = path.join(cssDir, file);
-
-    const data = fs.readFileSync(filePath, 'utf8');
-
+  if (extname(file) === '.css') {
+    const filePath = join(cssDir, file);
+    const data = readFileSync(filePath, 'utf8');
     const singleQuoteRegex = new RegExp(`url\\(\\s*'\\/(?!${repoURL})`, 'g');
     const doubleQuoteRegex = new RegExp(`url\\(\\s*"\\/(?!${repoURL})`, 'g');
-
     let result = data.replace(singleQuoteRegex, `url('/${repoURL}/`);
     result = result.replace(doubleQuoteRegex, `url("/${repoURL}/`);
-
-    fs.writeFileSync(filePath, result, 'utf8');
+    writeFileSync(filePath, result, 'utf8');
   }
 });
