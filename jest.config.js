5fdf6a2f8ced6161645104a4a2ace2040c9945de
Serhii Shymkiv <sergey@shimkiv.com>
2024-05-29 16:15:09 +0300

    Add coverage configuration and usage on CI.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,20 @@
 export default {
-  testMatch: ['<rootDir>/src/**/*.test.js'],
+  testMatch: ['**/src/**/*.test.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  collectCoverageFrom: [
+    '**/src/lib/**/*.js',
+    '!**/src/lib/ui/**',
+    '!**/src/**/*.test.js',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
