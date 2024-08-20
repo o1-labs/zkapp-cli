b307200b591a4677055891568a52de51df2fa9f0
boray <bsaygilier33@gmail.com>
2024-08-20 18:36:02 +0300

    Update jest and ts-jest versions

--- templates/project-ts/jest.config.js
+++ templates/project-ts/jest.config.js
@@ -3,14 +3,9 @@ export default {
   verbose: true,
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
   testTimeout: 1_000_000,
   transform: {
-    '^.+\\.(t)s$': 'ts-jest',
+    '^.+\\.(t)s$': ['ts-jest', { useESM: true }],
     '^.+\\.(j)s$': 'babel-jest',
   },
   resolver: '<rootDir>/jest-resolver.cjs',
