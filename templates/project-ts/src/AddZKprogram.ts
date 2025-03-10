812dfb569222df040adc55ba570f3d33d98c33fa
ymekuria <ymekuria@gmail.com>
2025-03-10 13:01:47 -0600

    feat(AddZKprogram.ts): add init method to the Add program

--- templates/project-ts/src/AddZKprogram.ts
+++ templates/project-ts/src/AddZKprogram.ts
@@ -3,4 +3,11 @@ import { ZkProgram, Field } from 'o1js';
 export const Add = ZkProgram({
   name: 'add-program',
   publicInput: Field,
+  methods: {
+    init: {
+      privateInputs: [],
+
+      async method() {},
+    },
+  },
 });
