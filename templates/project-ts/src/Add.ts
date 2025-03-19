b341071f61ae467b3577ea35feac8133c3f3844d
ymekuria <ymekuria@gmail.com>
2025-03-18 23:16:39 -0700

    refactor(Add.ts): update import statement for AddProgramProof

--- templates/project-ts/src/Add.ts
+++ templates/project-ts/src/Add.ts
@@ -1,5 +1,5 @@
 import { Field, SmartContract, state, State, method } from 'o1js';
-import { AddProgramProof } from './AddZKprogram';
+import { AddProgramProof } from './AddZKprogram.js';
 
 /**
  * Basic Example
