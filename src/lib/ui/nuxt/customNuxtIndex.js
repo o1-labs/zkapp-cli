0dff9e69b35047d018d6459086c29c8addb74197
ymekuria <ymekuria@gmail.com>
2025-03-18 23:32:29 -0700

    feat(customNuxtIndex.js): update deployed zkAppAddress

--- src/lib/ui/nuxt/customNuxtIndex.js
+++ src/lib/ui/nuxt/customNuxtIndex.js
@@ -126,7 +126,7 @@ export default {
 
       // Update this to use the address (public key) for your zkApp account.
       // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
-      // Testnet B62qnTDEeYtBHBePA4yhCt4TCgDtA4L2CGvK7PirbJyX4pKH8bmtWe5.
+      // Devnet B62qoptamt2EgyY2UM2WD7UVJRvuuDMxw2cLdENc92iVNm6FwsLX4Fk.
       const zkAppAddress = ''
       // This should be removed once the zkAppAddress is updated.
       if (!zkAppAddress) {
