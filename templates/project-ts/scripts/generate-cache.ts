b6c70b95badd027eccbb3a13f18634c8a7b100f4
ymekuria <ymekuria@gmail.com>
2025-04-08 23:56:12 -0700

    feat(generate-cache.ts): add cache_directory variable and initialize Cache.FileSystem with it

--- templates/project-ts/scripts/generate-cache.ts
+++ templates/project-ts/scripts/generate-cache.ts
@@ -1,3 +1,7 @@
 import { Cache } from 'o1js';
 
-const { Add } = await import('../build/src/Add.js')
+const { Add } = await import('../build/src/Add.js');
+
+const cache_directory = 'cache';
+
+const cache = Cache.FileSystem(cache_directory)
