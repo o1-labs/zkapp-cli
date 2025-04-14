cf8721b739619698fbe9ad30d058022e04d6fc0d
ymekuria <ymekuria@gmail.com>
2025-04-13 23:49:59 -0700

    feat(generate-cache.ts): compile contract with cache

--- templates/project-ts/scripts/generate-cache.ts
+++ templates/project-ts/scripts/generate-cache.ts
@@ -4,4 +4,6 @@ const { Add } = await import('../build/src/Add.js');
 
 const cache_directory = 'cache';
 
-const cache = Cache.FileSystem(cache_directory)
+const cache = Cache.FileSystem(cache_directory);
+
+const { verificationKey } = await Add.compile({ cache})
