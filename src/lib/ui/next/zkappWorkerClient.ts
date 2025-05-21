e47fe3d061a1bf1c8de120d3c326fa2cec329c82
ymekuria <ymekuria@gmail.com>
2025-05-20 23:55:48 -0700

    feat(zkappWorkerClient.ts): add initialization of worker from zkappWorker module using Worker API

--- src/lib/ui/next/zkappWorkerClient.ts
+++ src/lib/ui/next/zkappWorkerClient.ts
@@ -3,10 +3,11 @@ import * as Comlink from "comlink";
 export default class ZkappWorkerClient {
    worker: Worker;
 
-// Proxy to interact with the worker's methods as if they were local
+  // Proxy to interact with the worker's methods as if they were local
   remoteApi: Comlink.Remote<typeof import('./zkappWorker').api>; 
   
 constructor() {
-  
+  // Initialize the worker from the zkappWorker module
+  const worker = new Worker(new URL('./zkappWorker.ts', import.meta.url), { type: 'module' });  
 }  
 }
