aecff6a52a731d8b731d7e6a4744a578c5636705
ymekuria <ymekuria@gmail.com>
2025-05-20 23:43:17 -0700

    feat(zkappWorkerClient.ts): add worker client const

--- src/lib/ui/next/zkappWorkerClient.ts
+++ src/lib/ui/next/zkappWorkerClient.ts
@@ -4,5 +4,9 @@ export default class ZkappWorkerClient {
    worker: Worker;
 
 // Proxy to interact with the worker's methods as if they were local
-  remoteApi: Comlink.Remote<typeof import('./zkappWorker').api>;   
+  remoteApi: Comlink.Remote<typeof import('./zkappWorker').api>; 
+  
+constructor() {
+  
+}  
 }
