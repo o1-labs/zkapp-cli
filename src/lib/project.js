12fe320ec3a7e630144141245b2b601b4da7362b
ymekuria <ymekuria@gmail.com>
2025-11-25 15:32:41 -0300

    Use imported tsConfig template in project.js

--- src/lib/project.js
+++ src/lib/project.js
@@ -422,46 +422,7 @@ async function scaffoldNext(projectName) {
     path.join('ui', 'public', 'assets')
   );
 
-  const tsconfig = `{
-  "compilerOptions": {
-    "target": "es2021",
-    "module": "esnext",
-    "lib": ["dom", "dom.iterable", "esnext"],
-    "strict": true,
-    "strictPropertyInitialization": false, // to enable generic constructors, e.g. on CircuitValue
-    "skipLibCheck": true,
-    "forceConsistentCasingInFileNames": true,
-    "esModuleInterop": true,
-    "moduleResolution": "bundler",
-    "experimentalDecorators": true,
-    "emitDecoratorMetadata": true,
-    "allowJs": true,
-    "declaration": false,
-    "sourceMap": true,
-    "noFallthroughCasesInSwitch": true,
-    "allowSyntheticDefaultImports": true,
-    "isolatedModules": true,
-    "noEmit": true,
-    "incremental": true,
-    "resolveJsonModule": true,
-    "jsx": "preserve",
-    "useDefineForClassFields": false, 
-    "importHelpers": true,
-    "paths": {
-      "@/*": ["./src/*"]
-    },
-    "plugins": [
-      {
-        "name": "next"
-      }
-    ]
-  },
-  "include": ["next-env.d.ts", "**/*.ts", "**/*.tsx", ".next/types/**/*.ts"],
-  "exclude": ["node_modules"]
-}
-`;
-
-  fs.writeFileSync(path.join('ui', 'tsconfig.json'), tsconfig);
+  fs.writeFileSync(path.join('ui', 'tsconfig.json'), tsConfig);
 
   // Add a script to the package.json
   let x = fs.readJsonSync(path.join('ui', 'package.json'));
